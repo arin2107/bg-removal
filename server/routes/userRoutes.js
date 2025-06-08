58f200d7508f8f8d5893cdc60370ecf59934a2f2
ARIN VASHISHTHA <121510816+arin2107@users.noreply.github.com>
2025-06-08 22:38:50 +0530

    Webhooks Added

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { clerkWebhooks} from '../controllers/UserController.js';
+
+const userRouter = express.Router()
+
+userRouter.post('/clerk-webhooks', clerkWebhooks);  
+
+export default userRouter;
+
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/mongodb.js';
+import userRouter from './routes/userRoutes.js';
 
 // App config
 const PORT = process.env.PORT || 4000;
@@ -14,11 +15,8 @@ app.use(cors());
 
 // API Routes
 app.get('/', (req, res) => res.send("API is running"));
+app.use('/api/user', userRouter)
 
-// Connect to DB and then start server
-try {
-  await connectDB();
-  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-} catch (error) {
-  console.error('Failed to connect to DB:', error);
-}
+  app.listen(PORT, () => console.log(`Server is running on port ` +PORT));
+  
+ 
